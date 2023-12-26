61aa95cccc469e660ea0e507c60993a43c8bfa6c
Marono David <105996786+dmarono@users.noreply.github.com>
2023-12-26 14:27:40 +0000

    atualizado as informações do arquivo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi Mor tudo bem? Fiz uma página na internet! Se ficou bom dá um elogio. =D =D</h1>
+    return <h1>Oi Mor tudo bem? Fiz uma página na internet! Se ficou bom dá um elogio.</h1>
 }
 
 export default Home;
